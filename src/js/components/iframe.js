32a5fea54b3036a8fae8e594cbca841bf35d1b55
pascalmoser <pascalmoser@gmail.com>
2022-02-18 14:42:43 +0100

    iframe component

--- src/js/components/iframe.js
+++ src/js/components/iframe.js
@@ -1,3 +1,26 @@
+import {IFRAME_DEFAULTS} from '../constants/defaults';
+
 export default class Iframe {
 
+    constructor(tbElement, options = {}) {
+        this.options = Object.assign(IFRAME_DEFAULTS, options);
+        this.tbElement = tbElement;
+        this.iframe = this.tbElement.querySelector('iframe');
+
+        this.init();
+    }
+
+    init() {
+        if (!this.iframe) {
+            this.tbElement.classList.add(this.options.invalidClass);
+            return;
+        }
+
+        this.options.onLoad(this.iframe);
+
+        this.iframe.addEventListener('load', () => {
+            this.tbElement.classList.add(this.options.loadedClass);
+            this.options.onLoaded(this.iframe);
+        });
+    }
 }
---
+++ src/js/constants/defaults.js
@@ -0,0 +1,8 @@
+export const IFRAME_DEFAULTS = {
+    invalidClass: 'iframe-invalid',
+    loadedClass: 'iframe-loaded',
+    onLoad: (iframe) => {
+    },
+    onLoaded: (iframe) => {
+    },
+};

aa1c98d78ca16db7e2d7ebc00ec1e5f552b92357
pascalmoser <pascalmoser@gmail.com>
2022-02-18 13:13:17 +0100

    initial commit

---
+++ src/js/toolbox.js
@@ -0,0 +1,12 @@
+// components
+import GoogleMaps from './components/googleMaps';
+import GoogleOptOutLink from './components/googleOptOutLink';
+import Iframe from './components/iframe';
+import Video from './components/video';
+
+export {
+    GoogleMaps,
+    GoogleOptOutLink,
+    Iframe,
+    Video,
+};
